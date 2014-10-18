29524122713004a206ba463a65f8d63aaab733cc
Russell Wakefield <RJWakefield@gmail.com>
2014-10-18 15:23:02 -0400

    Some basic bootstrappin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def alert_for(text, options = {})
+    options[:class] =   'alert-info' unless options[:class]
+    alert_class     =   "alert #{options[:class]}"
+    button_options  =   { :class => 'close', :"data-dismiss" => 'alert'}
+    button          =   ActiveSupport::SafeBuffer.new
+    button          <<  content_tag(:button, "x", button_options)
+
+    content_tag(:div, :class => alert_class) do
+      button +
+        content_tag(:span) do
+        content_tag(:i, :class => 'icon-warning-sign', :style => 'margin-right: 1em;') do end + raw(text)
+      end
+    end
+  end
+
+  def flash_messages
+    flash.each.map { |k, v|
+      if k == :error || k == 'error'
+        alert_for(v, :class => 'alert-danger')
+      else
+        alert_for(v, :class => 'alert-info')
+      end
+    }.inject(:+)
+  end
+
 end
