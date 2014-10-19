3738e2b91a053051708628459a73e266e1d4654e
Kyle Stevens <kstevens715@gmail.com>
2014-10-19 11:55:49 -0400

    Refactor the javascript

--- app/assets/javascripts/cylinders.js
+++ app/assets/javascripts/cylinders.js
@@ -13,6 +13,10 @@ $( document ).ready(function() {
     return document.getElementById("cylinder");
   }
 
+  function reRender() {
+    renderer.render(scene, camera);
+  }
+
   init();
 
   cylinderElement().addEventListener('mousemove', onMouseMove, false );
@@ -54,25 +58,23 @@ $( document ).ready(function() {
     return $.parseJSON(repoJSON);
   }
 
-  function createCap(width, siloHeight, x, material) {
-    var capHeight = 0.30 * width;
-    var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.30 * width, 0.38 * width, capHeight, 30, 10, false), material);
-    var capPosition = siloHeight + (0.5 * capHeight) + yOffSet();
+  function createCap(siloHeight, x, material) {
+    var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(capHeight(), 0.38 * cylinderWidth(), capHeight(), 30, 10, false), material);
+    var capPosition = siloHeight + (0.5 * capHeight()) + yOffSet();
     cylinder.position.set(x, capPosition, 0);
     return cylinder
   }
 
-  function createTop(width, siloHeight, x, material) {
-    var capHeight = 0.30 * width;
-    var topHeight = 0.125 * width;
-    var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(3, 0.30 * width, topHeight, 30, 10, false), material);
-    var capPosition = siloHeight + capHeight + 0.5 * topHeight + yOffSet();
+  function createTop(siloHeight, x, material) {
+    var topHeight = 0.125 * cylinderWidth();
+    var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(3, capHeight(), topHeight, 30, 10, false), material);
+    var capPosition = siloHeight + capHeight() + 0.5 * topHeight + yOffSet();
     cylinder.position.set(x, capPosition, 0);
     return cylinder
   }
 
-  function createCylinder(width, height, x, material) {
-    var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.33 * width, 0.33 * width, height, 30, 10, false), material);
+  function createCylinder(height, x, material) {
+    var cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.33 * cylinderWidth(), 0.33 * cylinderWidth(), height, 30, 10, false), material);
     var yPosition = (height / 2) + yOffSet();
     cylinder.position.set(x, yPosition, 0);
     return cylinder
@@ -88,19 +90,26 @@ $( document ).ready(function() {
     objects.push(object);
   }
 
+  function capHeight() {
+    return 0.30 * cylinderWidth();
+  }
+
+  function cylinderWidth() {
+    return windowWidth() / 7;
+  }
+
   function addCylinders(scene, siloSiding, siloRoof) {
     var statData = stats();
-    var cylinderWidth = windowWidth() / 7;
     var maxLines = statData[0].lines_for_person;
     var scalar = (0.5 * windowHeight() )/maxLines;
 
     for (index = 0; index < 5; index++) {
       var metaData = statData[index];
       var siloHeight = scalar * metaData.lines_for_person;
-      var xLocation = (index - 2) * cylinderWidth;
-      addToScene(createCylinder(cylinderWidth, siloHeight, xLocation, siloSiding), metaData);
-      addToScene(createCap(cylinderWidth, siloHeight, xLocation, siloRoof), metaData);
-      addToScene(createTop(cylinderWidth, siloHeight, xLocation, siloRoof), metaData);
+      var xLocation = (index - 2) * cylinderWidth();
+      addToScene(createCylinder(siloHeight, xLocation, siloSiding), metaData);
+      addToScene(createCap(siloHeight, xLocation, siloRoof), metaData);
+      addToScene(createTop(siloHeight, xLocation, siloRoof), metaData);
     }
   }
 
@@ -134,7 +143,7 @@ $( document ).ready(function() {
     addCylinders(scene, siloSiding, siloRoof);
 
     var backgroundTexture = THREE.ImageUtils.loadTexture( 'images/field.jpg', {}, function() {
-      renderer.render(scene, camera);
+      reRender();
     });
     var backgroundMesh = new THREE.Mesh(
         new THREE.PlaneGeometry(windowWidth(), windowHeight(), 0),
